8d3d1e5df3b39a0670d593b8508f872047e9724f
Primo <pm.lyszczarz@gmail.com>
2013-05-25 15:13:38 +0200

    Preliminary specification of QueryResult class.

--- src/main/java/pw/edu/elka/rso/storage/QueryResult.java
+++ src/main/java/pw/edu/elka/rso/storage/QueryResult.java
@@ -1,7 +1,11 @@
 package pw.edu.elka.rso.storage;
 
+import java.nio.ByteBuffer;
+
 /**
  */
 public class QueryResult {
-
+    public long queryId;
+    public boolean result;
+    public ByteBuffer output;
 }
