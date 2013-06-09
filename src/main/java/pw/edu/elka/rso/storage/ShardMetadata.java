4e97463c9c4e43687193ae6f58e36223c8f44e40
unknown <Tadeo@Tadeo-Komputer.(none)>
2013-06-09 11:34:22 +0200

    Poprawka: zero jako rangeStart, rangeStop = kuku

--- src/main/java/pw/edu/elka/rso/storage/ShardMetadata.java
+++ src/main/java/pw/edu/elka/rso/storage/ShardMetadata.java
@@ -50,7 +50,7 @@ class PartitionMetadata implements Serializable, Comparable<PartitionMetadata> {
     {
         id = (long) (Math.random()*(1 << 60));
         if (rangeStop > rangeStart &&
-                rangeStop > 0 && rangeStart > 0)
+                rangeStop >= 0 && rangeStart >= 0)
         {
             hashRange[0] = rangeStart;
             hashRange[1] = rangeStop;
