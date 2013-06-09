598e7d59641d6f5125886cd315b6cc7672dd9802
unknown <Tadeo@Tadeo-Komputer.(none)>
2013-06-09 13:52:29 +0200

    Statystyki jako opcount.

--- src/main/java/pw/edu/elka/rso/storage/ShardStatistics.java
+++ src/main/java/pw/edu/elka/rso/storage/ShardStatistics.java
@@ -1,6 +1,49 @@
 package pw.edu.elka.rso.storage;
 
+import pw.edu.elka.rso.server.ShardDetails;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
 /** This class will hold local shard statistics like number of processed queries.
  */
 public class ShardStatistics {
+    public void incShardOpcount(int shardId) {
+        if (!shardOpcount.contains(new Integer(shardId)))
+        {
+            shardOpcount.put(shardId,1);
+        }
+        else
+        {
+            shardOpcount.put(shardId,shardOpcount.get(shardId)+1);
+        }
+    }
+    public void incShardOpcount(ShardDetails shard) {
+        incShardOpcount(shard.getId());
+    }
+    public void resetShardOpcount(int shardId) {
+        shardOpcount.put(shardId,0);
+    }
+    public void resetShardOpcount(ShardDetails shard) {
+        shardOpcount.put(shard.getId(),0);
+    }
+    public int getShardOpcount(int shardId) {
+        return shardOpcount.get(new Integer(shardId));
+    }
+    public int getShardOpcount(ShardDetails shard) {
+        return shardOpcount.get(new Integer(shard.getId()));
+    }
+    public ShardDetails getTheLeastLoaded(ArrayList<ShardDetails> shards) {
+        Integer op = getShardOpcount(shards.get(0).getId());
+        ShardDetails shard = shards.get(0);
+        for(ShardDetails sh: shards) {
+            if (op > getShardOpcount(sh))
+            {
+                op = getShardOpcount(sh);
+                shard = sh;
+            }
+        }
+        return shard;
+    }
+    private Hashtable<Integer,Integer> shardOpcount = new Hashtable<Integer,Integer>();
 }
