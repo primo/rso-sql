1c907db498cb269fc4d93095d07396003235a4c6
Primo <pm.lyszczarz@gmail.com>
2013-06-11 10:11:25 +0200

    Test updated to the newest API

--- src/main/java/pw/edu/elka/rso/client/LabTest.java
+++ src/main/java/pw/edu/elka/rso/client/LabTest.java
@@ -1,9 +1,12 @@
 package pw.edu.elka.rso.client;
 
 import pw.edu.elka.rso.logic.procedures.SerializedProcedureCall;
+import pw.edu.elka.rso.server.AbstractServer;
+import pw.edu.elka.rso.server.ShardDetails;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -21,54 +24,51 @@ public class LabTest {
     public static final String clusterGateId = "localhost";
 
     public static void main(String[] args) throws Exception {
-        String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-        Socket clientSocket = new Socket(clusterGateId, 2222);
+        ShardDetails clientServer = new ShardDetails(5000, 100, clusterGateId);
+        Socket clientSocket = AbstractServer.initConnectionToOtherShard(clientServer);
         ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         // Test table
         outToServer.writeObject("CreateTest");
-        modifiedSentence = inFromServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
+        ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+        Object data = ois.readObject();
+        System.out.println(data.toString());
 
         // Test table
         outToServer.writeObject("CreateTest2");
-        modifiedSentence = inFromServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
+        ois = new ObjectInputStream(clientSocket.getInputStream());
+        data = ois.readObject();
+        System.out.println(data.toString());
 
         for(int i = 0; i < 1000; ++i) {
             List<String> params = new ArrayList<>();
-            params.add(Integer.valueOf(i).toString());
-            params.add(Integer.valueOf(i + 1).toString());
-            params.add("TEsT"+i);
-            SerializedProcedureCall procedure = new SerializedProcedureCall("InsertIntoTest",params);
-            outToServer.writeObject(procedure);
-            modifiedSentence = inFromServer.readLine();
-            System.out.println("FROM SERVER: " + modifiedSentence);
-            procedure = new SerializedProcedureCall("InsertIntoTest2",params);
-            outToServer.writeObject(procedure);
-            modifiedSentence = inFromServer.readLine();
-            System.out.println("FROM SERVER: " + modifiedSentence);
+            String req = "InsertIntoTest " + Integer.valueOf(i).toString() + " " +
+                    Integer.valueOf(i + 1).toString() + " TEsT"+Integer.valueOf(i).toString();
+            outToServer.writeObject(req);
+            ois = new ObjectInputStream(clientSocket.getInputStream());
+            data = ois.readObject();
+            System.out.println(data.toString());
+
+            req = "InsertIntoTest2 " + Integer.valueOf(i).toString() + " " +
+                    Integer.valueOf(i + 1).toString() + " TEsT"+Integer.valueOf(i).toString();
+            outToServer.writeObject(req);
+            ois = new ObjectInputStream(clientSocket.getInputStream());
+            data = ois.readObject();
+            System.out.println(data.toString());
         }
 
         for (int i = 0 ; i < 50 ; ++i) {
-            List<String> params = new ArrayList<>();
-            params.add(Integer.valueOf(1000 - i).toString());
-            SerializedProcedureCall procedure = new SerializedProcedureCall("SelectFromTestWhere2",params);
-            outToServer.writeObject(procedure);
-            modifiedSentence = inFromServer.readLine();
-            System.out.println("FROM SERVER: " + modifiedSentence);
+            outToServer.writeObject("SelectFromTestWhere2 " + Integer.valueOf(1000 - i).toString());
+            ois = new ObjectInputStream(clientSocket.getInputStream());
+            data = ois.readObject();
+            System.out.println(data.toString());
         }
 
         for (int i = 0 ; i < 50 ; ++i) {
-            List<String> params = new ArrayList<>();
-            params.add(Integer.valueOf(500 - i).toString());
-            params.add(Integer.valueOf(510 + i).toString());
-            SerializedProcedureCall procedure = new SerializedProcedureCall("JoinExample",params);
-            outToServer.writeObject(procedure);
-            modifiedSentence = inFromServer.readLine();
-            System.out.println("FROM SERVER: " + modifiedSentence);
+            outToServer.writeObject("JoinExample "+Integer.valueOf(500 - i).toString()+" "+Integer.valueOf(500 + i).toString());
+            ois = new ObjectInputStream(clientSocket.getInputStream());
+            data = ois.readObject();
+            System.out.println(data.toString());
         }
 
         clientSocket.close();
